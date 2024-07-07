d981e03c412c95b265e97efd373c3acb7a403a86
kangaree <garyzkang@gmail.com>
2024-07-07 09:13:40 -0400

    https://github.com/gregrickaby/nextjs-github-pages?tab=readme-ov-file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // <=== enables static exports
+  basePath: "/1816",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
 
